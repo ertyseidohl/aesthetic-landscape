ad076099d23e9cc6fbbf0d3ee81e5d2a05ee7960
Ryan McVerry <ryan.mcverry@gmail.com>
2018-01-15 18:58:24 -0500

    mountains are shaded

--- stages/mountains.py
+++ stages/mountains.py
@@ -37,14 +37,11 @@ class Mountain:
         self.patches = []
         _walk(self)
 
-    def draw(self, draw,fill):
+    def draw(self, draw, fill):
         draw.polygon(self.outline, fill=fill)
         for patch in self.patches:
-
-            for i, row in enumerate(patch):
-                for j, tup in enumerate(row):
-                    if tup and tup[2]:
-                        draw.point((tup[0], tup[1]), fill=fill-1)
+            for pixel in patch:
+                draw.point(pixel, fill=fill-1)
 
     def outline_tail(self):
         return self.outline[len(self.outline) -1]
@@ -52,36 +49,54 @@ class Mountain:
     def height(self):
         return max(self.outline[0][1] - self.peak[1], self.outline_tail()[1])
 
-    def add_patch(self, weight):
+    def pixel_in_boundaries(self, x, y, exclusive=False):
 
-        width = 20
-        height = 20
-        patch = [[0 for _ in range(width)] for _ in range(height)]
+        dx = 10000
+        best = None
+        for point in self.outline:
+            deltax = abs(point[0] - x)
+            if deltax < dx:
+                best = point
+                dx = deltax
 
-        origin_x = self.peak[0] + 5
-        origin_y = self.peak[1] + 50
+        if exclusive:
+            return best[1] < y < self.horizon
+        return best[1] <= y <= self.horizon
 
-        for j in range(height):
-            start_x = 0
-            start_y = j
+    def add_patch(self, weight=None):
 
-            offset_bottom = math.ceil((random.triangular() * width) / 2)
-            offset_top = width - math.ceil((random.triangular() * width) / 2)
+        if weight is None:
+            start_pixel = self.peak
+            weight = 0
+        else:
+            start_y = self.peak[1]
+            start_y = start_y + (self.horizon - start_y) * random.triangular() * weight
 
-            i = 0
-            while start_y >= 0 and start_x >= 0:
-                if i > offset_bottom and i < offset_top:
-                    patch[start_y][start_x] = (origin_x + start_x, origin_y + start_y, 1)
-                else:
-                    patch[start_y][start_x] = (origin_x + start_x, origin_y + start_y, 0)
+            start_x = self.peak[0]
+            while self.pixel_in_boundaries(start_x, start_y, exclusive=True):
+                start_x = start_x - 1
 
-                i += 1
-                start_y = start_y - 1
-                start_x = start_x + 1
+            start_pixel = (start_x, start_y)
+
+        width = random.randint(4, 8)
+        max_length = math.ceil((self.horizon - self.peak[1]) / 2)
+        patch = []
+
+        top_offset = 0
+        bottom_offset = random.randint(6 + math.ceil(10 * weight), 10 + math.ceil(10 * weight))
+        for i in range(width):
+
+            top_offset = top_offset + random.choice([2, 1, 1, 0, -1, -1, -2])
+            bottom_offset = bottom_offset + random.choice([2, 1, 1, 0, -1, -1, -2])
+
+            for j in range(top_offset, bottom_offset):
+                x = start_pixel[0] + i - j
+                y = start_pixel[1] + i + j
+
+                if self.pixel_in_boundaries(x, y, exclusive=True):
+                    patch.append((x,y))
+
         self.patches.append(patch)
-
-    def shift_y(self, amount):
-        self.outline = [(xy[0], xy[1] + amount) for xy in self.outline]
 
 
 def mountains(layers, layer_factory, seed_obj):
@@ -99,42 +114,45 @@ def mountains(layers, layer_factory, seed_obj):
 
     for i, mountain_range in enumerate(mountain_ranges):
         for mountain in mountain_range.mountains:
-            # if random.choices([False, False, False, False, True]):
-            #     mountain.add_patch(random.random())
+            mountain.add_patch()
+            for _ in range(random.randint(0, 3)):
+                mountain.add_patch(random.random())
+
             mountain.draw(draw, random.choice((
                 colors.FG_LIGHT,
-                colors.FG_MID,
-                colors.FG_DARK,
             )))
     del draw
 
     return layer
 
+
 SLOPES = [
     (0, 0),
-    (4, 1),
-    (3, 1),
     (2, 1),
     (1, 1),
     (1, 2),
     (1, 3),
-    (1, 4)
+    (1, 4),
+    (1, 3),
+    (1, 2),
+    (2, 1),
 ]
 
 
 def _slope_start():
     slope_index = 0
     slope_stability = 0.
-    return slope_index, slope_stability
+    slope_gravity = random.choice([0.1, 0.15, 0.15, 0.2, 0.25])
+    return slope_index, slope_stability, slope_gravity
 
 
-def _slope_stability(slope_index, slope_stability):
+def _slope_stability(slope_index, slope_stability, slope_gravity):
 
-    if slope_stability + random.random() > 1.25:
-        slope_index = slope_index + random.choice([1, 1, 1, -1, 2, -2])
+    if slope_stability + random.random() > 1:
+        slope_index = slope_index + random.choice([1, 1, 1, -1, -1, 2, -2])
         slope_stability = 0
     else:
-        slope_stability += 0.1
+        slope_stability += slope_gravity
 
     slope_index = max(slope_index, 0)
     slope_index = min(slope_index, len(SLOPES) -1)
@@ -146,16 +164,16 @@ def _walk(mountain):
     mountain.outline = [mountain.peak]
 
     walk_xy = mountain.peak
-    slope_index, slope_stability = _slope_start()
+    slope_index, slope_stability, slope_gravity = _slope_start()
     while walk_xy[1] <= mountain.horizon:
-        slope_index, slope_stability = _slope_stability(slope_index, slope_stability)
+        slope_index, slope_stability = _slope_stability(slope_index, slope_stability, slope_gravity)
         walk_xy = (walk_xy[0] - SLOPES[slope_index][0], walk_xy[1] + SLOPES[slope_index][1])
         mountain.outline.append(walk_xy)
 
     walk_xy = mountain.peak
-    slope_index, slope_stability = _slope_start()
+    slope_index, slope_stability, slope_gravity = _slope_start()
     while walk_xy[1] <= mountain.horizon:
-        slope_index, slope_stability = _slope_stability(slope_index, slope_stability)
+        slope_index, slope_stability = _slope_stability(slope_index, slope_stability, slope_gravity)
         walk_xy = (walk_xy[0] + SLOPES[slope_index][0], walk_xy[1] + SLOPES[slope_index][1])
         mountain.outline.append(walk_xy)
 
